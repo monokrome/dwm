143ac174c4810c1ebd18b745e51bd92177193118
Bailey Stoner <monokrome@monokro.me>
2019-06-17 19:59:07 -0700

    Third times the charm?! :D

--- config.def.h
+++ config.def.h
@@ -60,26 +60,26 @@ static const Layout layouts[] = {
 
 /* commands */
 static const char *dmenucmd[] = {
-	"rofi"
-		 "-show" "run"
-		 "-modi" "run"
-		 "-location" "2"
-		 "-width" "85"
-		 "-lines" "6"
-		 "-line-margin" "0"
-		 "-line-padding" "1"
-		 "-separator-style" "none"
-		 "-font" "mono 10"
-		 "-columns" "9"
-		 "-bw" "0"
-		 "-disable-history"
-		 "-hide-scrollbar"
-		 "-color-window" "#222222, #222222, #b1b4b3"
-		 "-color-normal" "#222222, #b1b4b3, #222222, #005577, #b1b4b3"
-		 "-color-active" "#222222, #b1b4b3, #222222, #007763, #b1b4b3"
-		 "-color-urgent" "#222222, #b1b4b3, #222222, #77003d, #b1b4b3"
-		 "-kb-row-select" "Tab"
-		 "-kb-row-tab" ""
+	"rofi",
+		 "-show", "run",
+		 "-modi", "run",
+		 "-location", "2",
+		 "-width", "85",
+		 "-lines", "6",
+		 "-line-margin", "0",
+		 "-line-padding", "1",
+		 "-separator-style", "none",
+		 "-font", "mono 10",
+		 "-columns", "9",
+		 "-bw", "0",
+		 "-disable-history",
+		 "-hide-scrollbar",
+		 "-color-window", "#222222, #222222, #b1b4b3",
+		 "-color-normal", "#222222, #b1b4b3, #222222, #005577, #b1b4b3",
+		 "-color-active", "#222222, #b1b4b3, #222222, #007763, #b1b4b3",
+		 "-color-urgent", "#222222, #b1b4b3, #222222, #77003d, #b1b4b3",
+		 "-kb-row-select", "Tab",
+		 "-kb-row-tab", "",
 };
 
 static const char *termcmd[]  = { "alacritty", NULL };
